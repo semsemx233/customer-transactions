8679f292b5e2e070534c1cc9a32bab260dfaf5f8
semsemx233 <semsem.x233@gmail.com>
2024-07-13 19:22:59 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "@fortawesome/fontawesome-free/css/all.css"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CustomerContextProvider from './Context/CustomerContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CustomerContextProvider>
     <App />
+    </CustomerContextProvider>
   </React.StrictMode>
 );
 
